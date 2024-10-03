8afa1129a8f1699a2985c03c65b14e3be01d493a
Louis Weigel <privat@louisweigel.com>
2024-10-03 22:40:18 +0200

    initial commit + Add chocolate glazed cookies

--- src/client/kotlin/com/louisweigel/weigel/WeigelCreateClient.kt
+++
@@ -1,9 +0,0 @@
-package com.louisweigel.weigel
-
-import net.fabricmc.api.ClientModInitializer
-
-object WeigelCreateClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
--- src/main/kotlin/com/louisweigel/weigel/data_persistance/PlayerData.kt
+++
@@ -1,8 +0,0 @@
-package com.louisweigel.weigel.data_persistance
-
-class PlayerData {
-    var homeIsSet = false
-    var homeX = 0
-    var homeY = 0.0
-    var homeZ = 0
-}
